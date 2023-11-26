feba0ff3d7f9da6ea2a76d37515cda4a91db8b7d
DanielSchild25 <daniel.schild25@hotmail.com>
2023-11-26 16:57:27 +0100

    Event System

--- Brokengine/src/Brokengine/Application.cpp
+++ Brokengine/src/Brokengine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Brokengine/Events/ApplicationEvent.h"
+#include "Brokengine/Log.h"
+
 namespace Brokengine
 {
 	Application::Application()
@@ -14,6 +17,9 @@ namespace Brokengine
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		BE_TRACE(e);
+
 		while (true);
 	}
 }
--- Brokengine/src/Brokengine/Core.h
+++ Brokengine/src/Brokengine/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error Brokengine only supports Windows!
 #endif
+
+#define BIT(x) (1 << x)
